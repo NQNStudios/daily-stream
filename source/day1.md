b3aa7ad1e4763c71a2bfd52fa39bb9a339986fb4
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-05-22 11:45:24 -0400

    Adding the ability to show and scale sprites for a section

--- source/day1.md
+++ source/day1.md
@@ -2,10 +2,13 @@
 
 I'm back in New York City! This is the place where I first decided to start making Stream of Pretentiousness. Let me set the tone of [TODO the game/my life] by telling you an [anecdote]({@Day1-Anecdote}) about moving here.
 
-{{Day1-Anecdote}}
+{{Day1-Anecdote: Sprite, bunk, Scale, 0.3}}
 
-I'm only here for the next 3 months, and on a tight budget. This is where I sleep: 
+I'm only here for the next 3 months, and on a tight budget. I live in a shared bedroom with a total of 6 beds. I'm top bunk on the left, and I don't get much [privacy]({@Day1-AloneTime}).
 
-(TODO show apartment)
+{{Day1-AloneTime}}
 
-It's a shared bedroom with a total of 6 beds. I don't get much alone time.
+This is a group chat where I talk about very important topics.
+
+(TODO introducing no-fap Nat)
+
--- source/script.js
+++ source/script.js
@@ -1,5 +1,33 @@
 // Your Javascript goes here
 
+// Load assets
+function __preload() {
+  phaser.load.image('bunk', 'assets/bunk.jpg');
+}
+
+// New section event: if a section has the Sprite tag, add the sprite to the
+// window whose key matches the tag following Sprite
+Core.AddEventListener('OnGotoSection', function(id, element, tags, reason) {
+  if (window['sprite'] != null) {
+    window['sprite'].destroy();
+  }
+
+  var index = tags.indexOf('Sprite');
+  if (index != -1) {
+    var spriteKey = tags[index+1];
+
+    window['sprite'] = phaser.add.sprite(0, 0, spriteKey);
+  }
+
+  index = tags.indexOf('Scale');
+  var scale = 1;
+  if (index != -1) {
+    var scale = parseFloat(tags[index+1]);
+
+    window['sprite'].scale.setTo(scale, scale);
+  }
+});
+
 function Season2Links() {
   // Generate HTML links to each section 
   // TODO this could be a lot nicer if Core provided link generator functions
